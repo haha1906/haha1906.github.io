8806a73e1941a49b5967346b2bae638d4905c803
haha1906 <51917139+haha1906@users.noreply.github.com>
2021-03-29 13:39:16 +0200

    Delete Ziya/kisasulenbiya directory

--- Ziya/kisasulenbiya/js/book-names.js
+++
@@ -1,5 +0,0 @@
-var books = [
-  { name: "Genesis Comp", ref: "eni-01-GEN-001.html" },
-  { name: "Moses Comp", ref: "eni-02-EXO-001.html" },
-  { name: "Jonah Comp", ref: "eni-32-JON-001.html" },
-];
