66c13e1d343f7afb473ed866bc475a1dc6647d7e
haha1906 <51917139+haha1906@users.noreply.github.com>
2021-03-29 09:59:55 +0200

    Delete Ziya/kisasulenbiya directory

--- Ziya/kisasulenbiya/js/book-names.js
+++
@@ -1,7 +0,0 @@
-var books = [
-  { name: "Tekvin Derlemesi", ref: "tr-01-GEN-001.html" },
-  { name: "Yunus Derlemesi", ref: "tr-32-JON-001.html" },
-  { name: "The Story Of Adam (a.s.)", ref: "en-01-GEN-001.html" },
-  { name: "The Story Of Musa (a.s.)", ref: "en-02-EXO-001.html" },
-  { name: "Jon", ref: "en-32-JON-001.html" },
-];
