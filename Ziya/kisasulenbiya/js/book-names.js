69740560cc9b2e38f838a085e50cc5339bd9a706
haha1906 <51917139+haha1906@users.noreply.github.com>
2021-03-29 15:19:08 +0200

    Add files via upload

---
+++ Ziya/kisasulenbiya/js/book-names.js
@@ -0,0 +1,9 @@
+var books = [
+  { name: "Tekvin Derlemesi", ref: "tri-01-GEN-001.html" },
+  { name: "Yunus Derlemesi", ref: "tri-32-JON-001.html" },
+  
+    { name: "Genesis Compile", ref: "eni-01-GEN-001.html" },
+    { name: "Moses Compile", ref: "eni-02-EXO-001.html" },
+    { name: "Jonah Compile", ref: "eni-32-JON-001.html" },
+  
+];
